67f039059ca1255c39e8f20ca1b5df4d938707c7
JaeYoun Kim <rlawodusx@naver.com>
2024-08-07 23:51:57 +0900

    Feat: 애니메이션 추가 후 테스트

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private float moveSpeed = 4.0f;
     private Vector3 moveDirection = Vector3.zero;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
@@ -17,6 +25,16 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+
+        if(x > 0) {
+            animator.SetBool("isWalk", true);
+            spriteRenderer.flipX = false;
+        } else if(x < 0) {
+            spriteRenderer.flipX = true;
+            animator.SetBool("isWalk", true);
+        } else {
+            animator.SetBool("isWalk", false);
+        }
 
         // 이동 방향 설정
         moveDirection = new Vector3(x, y, 0);
